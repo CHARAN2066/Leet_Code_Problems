886a8440c84871486c9d74f5362dc21692bef356
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2025-10-13 22:22:29 +0530

    Time: 7 ms (48.17%) | Memory: 21.2 MB (10.52%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        queue<int> q;
+        vector<int> indegree(n);
+        vector<int> ans;
+        vector<vector<int>> adj(n);
+        for(auto i:edges)
+        {
+            adj[i[0]].push_back(i[1]);
+        }
+        for(auto i:edges)
+        {
+            indegree[i[1]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        int t;
+        while(!q.empty())
+        {
+            t=q.front();
+            ans.push_back(t);
+            for(auto i:adj[t])
+            {
+                indegree[i]--;
+                if(indegree[i]==0)
+                q.push(i);
+            }
+            q.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.size()!=n)
+        return false;
+        else
+        return true;
+    }
+};
