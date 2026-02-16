436424380cb040528c008af4011ea153b937861b
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2026-02-16 19:56:40 +0530

    Time: 24 ms (19.63%) | Memory: 22.9 MB (16.42%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-    int fun(int a,int b){
-        int c=a^b,ans=0;
-       return __builtin_popcount(c);
-    }
     int totalHammingDistance(vector<int>& nums) {
-        int n=nums.size(),ans=0;
+        int t,r,n=nums.size(),ans=0;
+        vector<pair<int,int>> b(32,{0,0});
+        vector<int> temp;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                ans+=fun(nums[i],nums[j]);
+            t=nums[i];
+            temp.clear();
+            while(t>0){
+                r=t%2;
+                temp.push_back(r);
+                t=t/2;
+            }         
+            while(temp.size()<32){
+                temp.push_back(0);
+            }   
+            reverse(temp.begin(),temp.end());
+            for(int i=0;i<32;i++){
+                if(temp[i]==0)
+                b[i].first++;
+                else
+                b[i].second++;
             }
+        }
+        for(int i=0;i<32;i++){
+            ans+=(b[i].first*b[i].second);
         }
         return ans;
     }
