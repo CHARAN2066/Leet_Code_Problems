3796cdd74e600e68664fe01ae09e340c647f807a
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2026-02-16 14:53:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (73.18%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        int a=0,r;
-        vector<int> b;
+        int ans=0,r;
+        int a=31;
         while(n>0){
             r=n%2;
-            b.push_back(r);
+            ans+=(r*(1<<a));
+            a--;
             n=n/2;
         }
-        while(b.size()<33){
-            b.push_back(0);
-        }
-        int ans=0;
-        for(int i=31;i>=0;i--)
-        {
-            ans+=(b[31-i]*(1<<i));
-        }
+        
         return ans;
     }
 };
