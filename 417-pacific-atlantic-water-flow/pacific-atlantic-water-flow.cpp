eaf641d6394d4797b27d1b1614bab1f7bd305c96
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2026-05-02 16:05:38 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (73.79%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,46 +1,52 @@
 class Solution {
 public:
-    void fun(vector<vector<int>>& heights,int i,int j,int n,int m,vector<vector<int>>& visted,bool &pa,bool &at){
+    vector<vector<int>> p;
+    void helper(vector<vector<int>> &heights,int i,int j,int n,int m,int flag){
         if(i>=n||j>=m||i<0||j<0)
         return;
-        if(visted[i][j]==1)
+        if(flag==1){
+            if(p[i][j]==1)
+            return;
+            p[i][j]=1;
+        }
+        else{
+            if(p[i][j]>1)
+            return;
+            p[i][j]+=2;
+        }
+        if((i+1<n)&&(heights[i+1][j]>=heights[i][j]))
+        helper(heights,i+1,j,n,m,flag);
+        if((j+1<m)&&(heights[i][j+1]>=heights[i][j]))
+        helper(heights,i,j+1,n,m,flag);
+        if((i-1>=0)&&(heights[i-1][j]>=heights[i][j]))
+        helper(heights,i-1,j,n,m,flag);
+        if((j-1>=0)&&(heights[i][j-1]>=heights[i][j]))
+        helper(heights,i,j-1,n,m,flag);
         return;
-        visted[i][j]=1;
-        if(i==0||j==0)
-        pa=true;
-        if(i==n-1||j==m-1)
-        at=true;
-        // cout<<"1";
-        if((i+1<n)&&(heights[i+1][j]<=heights[i][j]))
-        fun(heights,i+1,j,n,m,visted,pa,at);
-        if((j+1<m)&&(heights[i][j+1]<=heights[i][j]))
-        fun(heights,i,j+1,n,m,visted,pa,at);
-        if((i-1>=0)&&(heights[i-1][j]<=heights[i][j]))
-        fun(heights,i-1,j,n,m,visted,pa,at);
-        if((j-1>=0)&&(heights[i][j-1]<=heights[i][j]))
-        fun(heights,i,j-1,n,m,visted,pa,at);
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n=heights.size(),m=heights[0].size();
+        p.resize(n,vector<int>(m,0));
         vector<vector<int>> ans;
-        int n=heights.size();
-        int m=heights[0].size();
-        bool pa=false;
-        bool at=false;
-        vector<vector<int>> visted(n,vector<int> (m,0));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                pa=false;
-                at=false;
-                visted.assign(n,vector<int> (m,0));
-                fun(heights,i,j,n,m,visted,pa,at);
-                // cout<<pa<<" "<<at<<endl;
-                if(pa==true&&at==true)
-                ans.push_back({i,j});
-                // cout<<endl;
-            }
+        for(int i=0;i<n;i++){
+            helper(heights,i,0,n,m,1);
         }
+        for(int i=0;i<m;i++){
+            helper(heights,0,i,n,m,1);
+        } 
+        for(int i=0;i<n;i++){
+            helper(heights,i,m-1,n,m,2);
+        }
+        for(int i=0;i<m;i++){
+            helper(heights,n-1,i,n,m,2);
+        }     
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(p[i][j]==1||p[i][j]%2==0)
+                continue;
+                ans.push_back({i,j});
+            }
+        }   
         return ans;
     }
 };
