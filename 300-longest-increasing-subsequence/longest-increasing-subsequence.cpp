c0d30178487d5a0c59e7e2be02e132fbd79cc5bd
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2026-05-07 21:53:13 +0530

    Time: 62 ms (57.04%) | Memory: 14.2 MB (74.66%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int helper(int n,int i,int prev,vector<int>& nums,vector<vector<int>> &dp){
-        if(i==n)
-        return 0;
-        if(dp[i][prev+1]!=-1)
-        return dp[i][prev+1];
-        int pick=0,notpick=0;
-        if(prev==-1||nums[i]>nums[prev]){
-            pick=1+helper(n,i+1,i,nums,dp);
-        }
-        notpick=helper(n,i+1,prev,nums,dp); 
-        return dp[i][prev+1]=max(pick,notpick);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return helper(n,0,-1,nums,dp);
+        vector<int> dp(n,1);
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        int ans=0;
+        for(auto i:dp)
+        ans=max(ans,i);
+        return ans;
     }
 };
