5e4bd74475cd00e808c626ff56d2773d47b9ae18
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2026-06-22 15:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (15.37%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        map<char,int> mp;
+        for(auto i:text)
+        mp[i]++;
+        return min({mp['b'],mp['a'],mp['l']/2,mp['o']/2,mp['n']});
+        
+    }
+};
