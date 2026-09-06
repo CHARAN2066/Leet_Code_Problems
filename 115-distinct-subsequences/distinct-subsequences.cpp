8ac4e3a6e884fbd9c1740fb473f84e552d7520f1
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2026-09-06 18:13:13 +0530

    Time: 35 ms (34.20%) | Memory: 26.8 MB (72.62%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int helper(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
+        if (j == t.size()) {
+            return 1;
+        }
+        if (i >= s.size() || j >= t.size())
+        return 0;
+        if (dp[i][j] != -1)
+        return dp[i][j];
+        int ans = 0;
+        if (s[i] == t[j]) {
+            ans += helper(i + 1, j + 1, s, t, dp);
+        }
+        ans += helper(i + 1, j, s, t, dp);
+        return dp[i][j] = ans;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return helper(0, 0, s, t, dp);
+    }
+};
